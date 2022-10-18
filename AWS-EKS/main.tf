c4e35ceda1eba58746d422095d2ba47796e689f9
ROYROHITH06 <98080097+ROYROHITH06@users.noreply.github.com>
2022-10-18 12:18:36 +0530

    Update main.tf

--- AWS-EKS/main.tf
+++ AWS-EKS/main.tf
@@ -178,9 +178,9 @@ resource "aws_eks_node_group" "node" {
   subnet_ids      = aws_subnet.demonode[*].id
 
   scaling_config {
-    desired_size = 6
-    max_size     = 6
-    min_size     = 2
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
  }
 
   ami_type       = "AL2_x86_64"
